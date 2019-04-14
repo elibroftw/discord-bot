c37165f931e2959a1f08861ce3e222e2a6763f26
Elijah Lopez <elijahllopezz@gmail.com>
2019-04-14 19:41:07 -0400

    Update bot.py
    Former-commit-id: c69ac1a981f32760e11c920a186eedfdc5a4bcc9

--- bot.py
+++ bot.py
@@ -26,6 +26,7 @@ handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w'
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 bot = commands.Bot(command_prefix='!')
+bot.remove_command('help')
 bot.command()
 load_opus_lib()
 
