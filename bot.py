1f88d4898790c39a6888e566a3fb68762ecf303b
Elijah Lopez <elijahllopezz@gmail.com>
2020-11-18 21:47:52 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -53,7 +53,10 @@ logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
-bot = commands.Bot(command_prefix='!')
+
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix='!', intents=intents)
 bot.remove_command('help')
 bot.command()
 load_opus_lib()
