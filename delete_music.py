9e5750e64483873eda576ab0623b7747c2aeccf4
Elijah Lopez <elijahllopezz@gmail.com>
2020-12-12 12:55:37 -0500

    Update delete_music.py

--- delete_music.py
+++ delete_music.py
@@ -2,7 +2,7 @@ from shutil import rmtree
 from os import mkdir
 
 print('Deleting Music Directory... ')
-rmtree('Music')
+rmtree('music')
 print('Creating Music Directory...')
-mkdir('Music')
+mkdir('music')
 print('Done!')
