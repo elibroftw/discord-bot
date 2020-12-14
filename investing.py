026eb42d52df17c5c8c0ab1243ab6eb987f27ccc
Elijah Lopez <elijahllopezz@gmail.com>
2020-12-14 12:30:41 -0500

    imrpove formatting

--- investing.py
+++ investing.py
@@ -37,14 +37,18 @@ def time_cache(max_age, maxsize=128, typed=False):
         maxsize: Maximum cache size (see `functools.lru_cache`).
         typed: Cache on distinct input types (see `functools.lru_cache`).
     """
+
     def _decorator(fn):
         @functools.lru_cache(maxsize=maxsize, typed=typed)
         def _new(*args, __time_salt, **kwargs):
             return fn(*args, **kwargs)
+
         @functools.wraps(fn)
         def _wrapped(*args, **kwargs):
             return _new(*args, **kwargs, __time_salt=int(time.time() / max_age))
+
         return _wrapped
+
     return _decorator
 
 
